3c8d70345adb19bef7176a75aefc61a0107769aa
sanghavi2431 <sanghavi2431@gmail.com>
2025-10-28 14:54:05 +0530

    Update Applicationstart.sh

--- scripts/Applicationstart.sh
+++ scripts/Applicationstart.sh
@@ -2,7 +2,7 @@
 
 set -e 
 
-cd /home/ubuntu/my-medusa-store-1
+cd /home/ubuntu/medusa-server/medusa_ecommerce_store-1
 
 echo "Starting Medusa with PM2"
 pm2 start npm --name medusa-server -- run dev 
